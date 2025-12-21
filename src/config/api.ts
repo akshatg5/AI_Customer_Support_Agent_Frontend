3feedc2365e4be2c745e53695c86f9f88a136393
akshatg5 <akshatgirdhar5@gmail.com>
2025-12-21 19:00:12 +0530

    fix : api prod

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,3 @@
-// export const BASE_API_URL = "https://ai-customer-support-agent-backend.vercel.app";
-export const BASE_API_URL = "http://localhost:3000";
+export const BASE_API_URL =
+  "https://ai-customer-support-agent-backend.vercel.app";
+// export const BASE_API_URL = "http://localhost:3000";
